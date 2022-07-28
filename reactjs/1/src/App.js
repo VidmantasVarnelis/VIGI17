f29eebbb0a0c6a2687c81f0c961d529fb62e02be
Vidmantas Varnelis <Vidmantas.X.Varnelis@aib.ie>
2022-07-28 17:56:26 +0300

    Paskaita5

--- reactjs/1/src/App.js
+++ reactjs/1/src/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import Paskaita2 from './paskaita2/Paskaita2';
-import Uzduotis2 from './paskaita2/Uzduotis2';
-import Paskaita4 from './paskaita4/Paskaita4';
-import Paskaita4Class from './paskaita4/Paskaita4Class';
+// import Paskaita2 from './paskaita2/Paskaita2';
+// import Uzduotis2 from './paskaita2/Uzduotis2';
+// import Paskaita4 from './paskaita4/Paskaita4';
+// import Paskaita4Class from './paskaita4/Paskaita4Class';
+import Paskaita5 from './paskaita5/Paskaita5';
 
 function App() {
   return (
     <div>
       {/* <Paskaita2 /> */}
       {/* <Uzduotis2 /> */}
-      <Paskaita4 />
-      <Paskaita4Class />
+      {/* <Paskaita4 />
+      <Paskaita4Class /> */}
+      <Paskaita5 />
     </div>
   );
 }
